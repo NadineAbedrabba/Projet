e3ca283effa4df950db4f7b97df173d88615c4bc
NadineAbedrabba <150744455+NadineAbedrabba@users.noreply.github.com>
2025-01-01 22:13:25 +0100

    Delete stage front directory

--- stage front/src/app/model/Competence.model.ts
+++
@@ -1,9 +0,0 @@
-import { Domaine } from "./Domaine.model";
-
-export class Competence {
-    nomCompetence!: string ; 
- scoreCompetence ! : number ;
- niveauCompetence ! : string ;
-	
-  domaine  !: Domaine ; 
-}
